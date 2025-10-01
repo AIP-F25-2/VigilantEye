6dcb5a9c4008544b8ec6b9de7f31f5b07e2e2082
Sri Datta Reddy <sridattareddy92@gmail.com>
2025-09-30 20:24:43 -0400

    Add Sonar workflow and notebooks

---
+++ sample_bad.py
@@ -0,0 +1,14 @@
+﻿import subprocess  # unused import
+PASSWORD = "12345"  # hardcoded credential
+
+def bad_compare(x, y=[]):  # mutable default (bug)
+    if x == None:          # bad None comparison
+        pass
+
+def dangerous(cmd):
+    subprocess.run(cmd, shell=True)  # shell=True is a security risk
+
+try:
+    1/0
+except:
+    pass  # swallowing all exceptions (code smell)
